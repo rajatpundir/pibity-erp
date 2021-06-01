de1043e599adadf1cc40bd535ece8957a98cebaf
Kavita Goel <kavita.goel@pibity.com>
2021-06-01 12:35:42 +0530

    fill dummy data for supplier and product

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -47,21 +47,10 @@ import ListWarehouseReceipt from '../components/Warehouse/Receipt/List'
 import { executeCircuit } from './circuit'
 import { circuits } from './circuits'
 import { getState } from './store'
+import TrialDataExecute from './TrialDataExecute'
 
 function App() {
-  // const [a, b, c] = executeCircuit(circuits.createProduct, {
-  //   sku: 'abc',
-  //   name: 'name',
-  //   orderable: true,
-  //   consumable: true,
-  //   producable: true,
-  //   uoms: [{
-  //     name: 'kg',
-  //     conversionRate: 1
-  //   }]
-  // })
-  // console.log(a, b)
-  // getState().addDiff(c)
+    TrialDataExecute()
   return (
     <div className='App font-nunito bg-gray-100'>
       <BrowserRouter>
